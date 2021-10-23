75e1bb3b02919c8fdf157b53fdf35e6c6c7ce687
taha-shafique <80164015+taha-shafique@users.noreply.github.com>
2021-10-23 09:28:04 -0400

    Create gpu_info.py
    provides information on the colab runtime and gpu

---
+++ gpu_info.py
@@ -0,0 +1,15 @@
+gpu_info = !nvidia-smi
+gpu_info = '\n'.join(gpu_info)
+if gpu_info.find('failed') >= 0:
+  print('Not connected to a GPU')
+else:
+  print(gpu_info)
+
+from psutil import virtual_memory
+ram_gb = virtual_memory().total / 1e9
+print('Your runtime has {:.1f} gigabytes of available RAM\n'.format(ram_gb))
+
+if ram_gb < 20:
+  print('Not using a high-RAM runtime')
+else:
+  print('Using a high-RAM runtime!')
